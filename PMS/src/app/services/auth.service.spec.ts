64fb50b12a48a4e838c52d009639ab1dd6024595
FavourGaitano <favourbuyanzi@gmail.com>
2024-03-02 20:54:21 +0300

    cypress

--- PMS/src/app/services/auth.service.spec.ts
+++ PMS/src/app/services/auth.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
+import { registerDetails } from '../interfaces/register.interfaces';
+
+
 
 describe('AuthService', () => {
   let service: AuthService;
+  let testingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AuthService);
+    testingController = TestBed.inject(HttpTestingController)
   });
+
+  afterEach(()=> {testingController.verify()})
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('registers a user', ()=>{
+    let mockUser: registerDetails = {
+      name: "user",
+      email: "user@yopmail.com",
+      password: "user"
+
+    }
+
+    service.registerUser(mockUser).subscribe(res=>{
+      expect(res.message).toEqual("Account created successfully")
+    })
+
+    const mockReq = testingController.expectOne('http://localhost:4100/users');
+    expect(mockReq.request.method).toEqual('POST')
+    expect(mockReq.request.body).toBe(mockUser)
+    mockReq.flush({"message": "Account created successfully"})
+  })
 });
